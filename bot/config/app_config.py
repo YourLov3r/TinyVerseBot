a9eca5848d5c8d35e1d0a49d233672cf504e7a5b
Dellenoam <d.spiridonov.0256@gmail.com>
2024-12-16 11:53:01 +0500

    ✨ Update known TinyVerse version

--- bot/config/app_config.py
+++ bot/config/app_config.py
@@ -20,7 +20,7 @@ class UrlConfig(BaseModel):
 
 class AppSettings(BaseModel):
     urls: UrlConfig = UrlConfig()
-    KNOWN_VERSION: str = "0.7.21"
+    KNOWN_VERSION: str = "0.7.23"
 
 
 app_settings = AppSettings()
